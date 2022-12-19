480fc82376c11c45e5dfaff424fc09d7e6ea466e
Samuel Ssekizinvu <73385715+samuelkchris@users.noreply.github.com>
2022-12-19 16:06:49 +0300

    test file

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,73 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list - prints information about a Python list object
+ * @p: pointer to a Python object
+ */
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t size, i;
+    PyObject *item;
+
+    if (!PyList_Check(p))
+    {
+        printf("Error: Invalid list object\n");
+        return;
+    }
+
+    size = PyList_Size(p);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
+/**
+ * print_python_bytes - prints information about a Python bytes object
+ * @p: pointer to a Python object
+ */
+void print_python_bytes(PyObject *p)
+{
+    Py_ssize_t size, i;
+    char *data;
+
+    if (!PyBytes_Check(p))
+    {
+        printf("Error: Invalid bytes object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    data = PyBytes_AsString(p);
+    printf("[.] bytes object info\n");
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", data);
+    if (size > 10)
+        size = 10;
+    else
+        size++;
+    printf("  first %ld bytes: ", size);
+    for (i = 0; i < size; i++)
+        printf("%02hhx%c", data[i], i + 1 < size ? ' ' : '\n');
+}
+
+/**
+ * print_python_float - prints information about a Python float object
+ * @p: pointer to a Python object
+ */
+void print_python_float(PyObject *p)
+{
+    if (!PyFloat_Check(p))
+    {
+        printf("Error: Invalid float object\n");
+        return;
+    }
+
+    printf("[.] float object info\n");
+    printf("  value: %f\n", PyFloat_AsDouble(p));
+}
