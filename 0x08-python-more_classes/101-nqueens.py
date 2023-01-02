ab1d66255729b32a76c295e9f22cc69adbf61160
Samuel Ssekizinvu <73385715+samuelkchris@users.noreply.github.com>
2023-01-03 01:26:51 +0300

    test file

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """Solves the N-queens puzzle"""
-
 import sys
-
-
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
     return (board)
-
 
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
