cf0e08e768b6ebdacc42451d5887940c812b5846
samuelkchris <samuelkchirs>
2023-01-03 15:56:01 -0800

    little fix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(s=[]):
-    s += ["Holberton"]
-    return ", ".join(s)
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])
