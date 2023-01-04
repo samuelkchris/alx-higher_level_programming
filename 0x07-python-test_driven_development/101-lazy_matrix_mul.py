bb67309753739f0ea944d5a88addcdbca8ed757a
Samuel Ssekizinvu <73385715+samuelkchris@users.noreply.github.com>
2023-01-04 17:57:36 +0300

    test files

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-Module 101-lazy_matrix_mul
-Contains method to multiply matrix using numpy module (pip3 install numpy)
-https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.matmul.html
-"""
-import numpy
+"""Defines a matrix multiplication function using NumPy."""
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Returns matrix multiplied"""
-    return numpy.matmul(m_a, m_b)
+    """Return the multiplication of two matrices.
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
