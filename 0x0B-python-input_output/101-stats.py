91eb83a59d5e5658abcbfe9552bf3f058113a4c7
Samuel Ssekizinvu <73385715+samuelkchris@users.noreply.github.com>
2023-01-11 01:58:31 +0300

    Add files via upload

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
+"""
+Script reads stdin line by line and computes metrics
 
-if __name__ == "__main__":
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
 
-    import sys
+Each 10 lines and after a keyboard interruption (CTRL + C),
+prints those statistics since the beginning:
+total file size and
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
 
+format: File size: <total size>
+format: <status code (in ascending order)>: <number>
+"""
+
+
+import sys
+
+
+def print_size_and_codes(size, stat_codes):
+    print("File size: {:d}".format(size))
+    for k, v in sorted(stat_codes.items()):
+        if v:
+            print("{:s}: {:d}".format(k, v))
+
+
+def parse_stdin_and_compute():
     size = 0
-    statuscd = {}
+    lines = 0
+    stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                  "403": 0, "404": 0, "405": 0, "500": 0}
+    try:
+        for line in sys.stdin:
+            fields = list(map(str, line.strip().split(" ")))
+            size += int(fields[-1])
+            if fields[-2] in stat_codes:
+                stat_codes[fields[-2]] += 1
+            lines += 1
+            if lines % 10 == 0:
+                print_size_and_codes(size, stat_codes)
+    except KeyboardInterrupt:
+        print_size_and_codes(size, stat_codes)
+        raise
 
-    while True:
-        try:
-            counter = 0
-            for line in sys.stdin:
-                line = line.split()
+    print_size_and_codes(size, stat_codes)
 
-                size += int(line[8])
-                if statuscd.get(line[7], -1) == -1:
-                    statuscd[line[7]] = 1
-                else:
-                    statuscd[line[7]] += 1
 
-                if counter == 10:
-                    break
-                counter += 1
-
-        except KeyboardInterrupt:
-            exit()
-        finally:
-            print("File size: {}".format(size))
-            for key in sorted(statuscd):
-                print("{}: {}".format(key, statuscd[key]))
+parse_stdin_and_compute()
