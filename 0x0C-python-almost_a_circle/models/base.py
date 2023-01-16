44b069284993972dab417700b898a3738a35fb4f
samuelkchris <samuelkchirs>
2023-01-16 12:51:09 -0800

    reverted

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ import turtle
 class Base:
     """Base model.
 
-    Tis Represents the "base" for all other classes in project 0x0C*.
+    This Represents the "base" for all other classes in project 0x0C*.
 
     Private Class Attributes:
         __nb_object (int): Number of instantiated Bases.
